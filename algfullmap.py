db2f4090535051292d162dd769f89f66fcd2e2af
EdanSneh <edan.sneh@gmail.com>
2017-07-29 23:41:48 -0700

    modified circle finding algorithm, circle bug

--- algfullmap.py
+++ algfullmap.py
@@ -17,20 +17,42 @@ class mapalgorithm:
 
     def detectcircles(self):
         #TODO array not using strs
-        circleit = [[""]*np.size(self.datamap.map,0) for _ in range(np.size(self.datamap.map,1))]
-
+        circleit = [[" "]*np.size(self.datamap.map,0) for _ in range(np.size(self.datamap.map,1))]
+        self.names = 0
         indexrow = 0
         for row in self.secretmap:
             indexcol = 0
             for rowval in row:
                 if rowval < 24:
+
                     circleit[indexrow][indexcol] = "null"
-                    self.namecheck()
+
                 elif rowval >= 24 and rowval <= 60:
-                    self.namecheck()
-                    circleit[indexrow][indexcol] = "c1_ex"
+
+                    circleit[indexrow][indexcol] = "edge"
                 else:
-                    circleit[indexrow][indexcol] = "c1_in"
-                print circleit
+                    sstir = self.namecheck(circleit, indexrow, indexcol)
+                    circleit[indexrow][indexcol] = sstir
+
                 indexcol += 1
             indexrow += 1
+        print circleit
+
+    def namecheck(self, array, row, col):
+        lamebool = False
+        if row!=len(array) and array[row+1][col][0] == "c":
+            lamebool = True
+            savearray = array[row+1][col]
+        elif col!=0 and array[row][col-1][0] == "c":
+            lamebool = True
+            savearray = array[row][col-1]
+        elif col!=len(array[row]) and array[row][col+1][0] == "c":
+            lamebool = True
+            savearray = array[row][col+1]
+        elif row!=0 and col!=array[row-1][col][0] == "c":
+            lamebool = True
+            savearray = array[row-1][col]
+        if lamebool:
+            return savearray
+        self.names += 1
+        return "c"+str(self.names)+"_i"

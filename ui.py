bc77f7d06ea6cffd6e005f6780d340da125731c6
EdanSneh <edan.sneh@gmail.com>
2017-08-03 00:05:04 -0700

    little bit of ui work

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 from algfullmap import Mapalgorithm
+from pyglet.window import key
 import pyglet
 
 window = pyglet.window.Window()
@@ -7,10 +8,20 @@ label = pyglet.text.Label('Hello, world',
                           font_size=36,
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
+# image = pyglet.resource.image('kitten.jpg')
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.Q:
+        print "quit"
 
+platform = pyglet.window.get_platform()
+display = platform.get_default_display()
+print display.get_default_screen()
 @window.event
 def on_draw():
     window.clear()
+    # image.blit(0,0)
     label.draw()
+    # pyglet.graphics.draw(1, GL_POINTS, ('v2i',(10,20)))
 
 pyglet.app.run()
